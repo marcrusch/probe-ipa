fbaf46600fe0d413d3ecb4da7ec3a5685c6c0acf
MrRushCH <marc.rusch@hotmail.ch>
2021-11-25 11:31:41 +0100

    Edit default datepicker to show availability

--- src/components/Availability.js
+++ src/components/Availability.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import { CalendarPicker, LocalizationProvider, PickersDay } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { addDays } from "date-fns";
+import { Grid } from "@mui/material";
 
 export default function Availability({ lendPeriods }) {
+  const [displayOverlay, setDisplayOverlay] = useState(false);
+  const unavailableDays = [];
   let dotAvailability = "available";
   if (lendPeriods && lendPeriods.length) {
     const currentTimestamp = new Date().getTime();
@@ -26,16 +32,80 @@ export default function Availability({ lendPeriods }) {
         dotAvailability = "future-unavailable";
       }
     }
+
+    lendPeriods.forEach((lendPeriod) => {
+      const dayDiff = Math.floor(
+        (lendPeriod.endTs - lendPeriod.startTs) / (1000 * 60 * 60 * 24)
+      );
+      const startDate = new Date(lendPeriod.startTs);
+      const daysToColor = [];
+
+      for (let i = 1; i <= dayDiff; i++) {
+        daysToColor.push(addDays(startDate, i).setHours(0, 0, 0, 0));
+      }
+      unavailableDays.push(...daysToColor);
+    });
   }
+
   return (
     <>
-      <div className="availability">
+      <div
+        className="availability"
+        onMouseEnter={() => {
+          setDisplayOverlay(true);
+        }}
+        onMouseLeave={() => {
+          setDisplayOverlay(false);
+        }}
+      >
         <div
           className={`availability__dot availability__dot--state-${dotAvailability}`}
         ></div>
-        <div className="availability__overview"></div>
+        {displayOverlay && (
+          <div className="availability__overview">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <CalendarPicker
+                onChange={() => {}}
+                onDaySelect={() => {}}
+                outsideCurrentMonth={false}
+                readOnly
+                renderDay={(day) => {
+                  if (unavailableDays.indexOf(day.getTime()) !== -1) {
+                    return (
+                      <PickersDay
+                        day={day}
+                        onChange={() => {}}
+                        onDaySelect={() => {}}
+                        outsideCurrentMonth={false}
+                        style={{ backgroundColor: "#f00" }}
+                        key={day}
+                      />
+                    );
+                  } else {
+                    return (
+                      <PickersDay
+                        day={day}
+                        day={day}
+                        onChange={() => {}}
+                        onDaySelect={() => {}}
+                        outsideCurrentMonth={false}
+                        key={day}
+                      />
+                    );
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        )}
       </div>
       <style jsx>{`
+        .availability {
+          width: 100%;
+          height: 100%;
+          position: relative;
+        }
+
         .availability__dot--state-available {
           background-color: #00ff00;
         }
@@ -47,9 +117,24 @@ export default function Availability({ lendPeriods }) {
         }
 
         .availability__dot {
+          position: absolute;
           width: 10px;
           height: 10px;
           border-radius: 50%;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        .availability__overview {
+          top: 0;
+          left: 50%;
+          transform: translate(-50%);
+          position: absolute;
+          background-color: #fff;
+          border: 1px solid #444;
+          z-index: 10;
+          border-radius: 10px;
         }
       `}</style>
     </>
